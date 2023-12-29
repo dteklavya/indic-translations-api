1a688605f31f319da81f9a849b9604995c8bd2c8
Rajesh P <rajesh.pethe@gmail.com>
2023-12-29 19:17:28 +0530

    Get ULCA pipeline.

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from translate import BhashiniTranslator
 
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def main():
-    return {'msg': "Welcome!"}
+def home():
+    return {"msg": "Welcome!"}
+
+
+@app.route("/translate", methods=["POST"])
+def translate():
+    sourceLanguage = request.json.get("sourceLanguage")
+    targetLanguage = request.json.get("targetLanguage")
+    translator = BhashiniTranslator()
+    translator.getPipeLine(sourceLanguage, targetLanguage)
+    return jsonify(translator.pipeLineData)
 
 
 if __name__ == "__main__":
