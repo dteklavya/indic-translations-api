f76af5ae9f298fbadd0658c1602559a6c5f4fa03
Rajesh P <rajesh.pethe@gmail.com>
2024-01-11 10:35:26 +0530

    TTS and ASR end-points.

--- controllers/bhashini.controllers.mjs
+++ controllers/bhashini.controllers.mjs
@@ -1,4 +1,7 @@
 import bhashini from "bhashini-translation"
+import { tts_payload } from "bhashini-translation/config/payload.js"
+import BaseApi from "bhashini-translation/src/utils/baseApi.js"
+import fs from 'fs'
 
 export const nmt = async (req, res) => {
     const sourceLanguage = req.body.sourceLanguage
@@ -13,4 +16,41 @@ export const nmt = async (req, res) => {
     }
 }
 
-export default nmt
+
+export const asr = async (req, res) => {
+    const sourceLanguage = req.body.sourceLanguage
+    const base64String = "VWtsR1JuSVFCZ0JYUVZaRlptMTBJQklBQUFBREFBRUFJbFlBQUloWUFRQUVBQ0FBQUFCbVlXTjBCQUFBQUJDRUFRQmtZWFJoUUJBR0FMellZenBkalZFNkxKSnZPZ3pNWWpxclpYNDZuUU5kT2t3bWJUcThGMmc2Cg=="
+    try {
+        const result = await bhashini.asr(sourceLanguage, base64String)
+
+        res.status(200).json(result)
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
+
+export const tts = async (req, res) => {
+    const sourceLanguage = req.body.sourceLanguage
+    const text = req.body.text
+    const gender = req.body.gender
+
+    const bhashiniApi = new BaseApi;
+    const payload = await tts_payload(sourceLanguage, text, gender)
+    try {
+        const response = await bhashiniApi.post(payload)
+        const result = await bhashini.tts(sourceLanguage, text, gender)
+        let base64Sound = response.data.pipelineResponse[0].audio[0].audioContent;
+
+        const buffer = Buffer.from(base64Sound, 'base64')
+        const filename = `/tmp/audio_${Date.now()}.wav`;
+
+        fs.writeFileSync(filename, buffer);
+
+        // Send the file as the response
+        res.status(200).sendFile(filename);
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
+
+export default nmt 
