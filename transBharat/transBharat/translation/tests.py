064da691f93e71e23773a42924e5fdb1f343e085
Rajesh P <rajesh.pethe@gmail.com>
2024-03-30 06:27:37 +0530

    Tests.

--- transBharat/transBharat/translation/tests.py
+++ transBharat/transBharat/translation/tests.py
@@ -159,3 +159,16 @@ class TestTranslationAPI(TestCase):
             self.assertIsNotNone(response)
             self.assertEqual(response.status_code, status.HTTP_200_OK)
             self.assertEqual(response.data, b"mock base64 encoded string")
+
+    def test_auth_required_nmt_tts(self):
+        response = self.client.post(
+            "/api/nmt_tts/",
+            {
+                "sourceLanguage": "en",
+                "targetLanguage": "hi",
+                "text": "hello",
+                "gender": "female",
+            },
+        )
+        self.assertIsNotNone(response)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
