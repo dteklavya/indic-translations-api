7d85dabd97dd51f743d40b6312b52243d2de0375
Rajesh P <rajesh.pethe@gmail.com>
2024-04-05 19:08:50 +0530

    Swagger views for API documentation.

--- transBharat/transBharat/urls.py
+++ transBharat/transBharat/urls.py
@@ -18,6 +18,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt import views as jwt_views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Episyche Technologies",
+        default_version="v1",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -27,4 +40,9 @@ urlpatterns = [
         "api/token/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refesh"
     ),
     path("api/", include("transBharat.translation.urls"), name="translation_api"),
+    path(
+        "api-docs/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
