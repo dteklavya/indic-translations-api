e5714bae936b538abc520c029da7462c4343312c
Rajesh P <rajesh.pethe@gmail.com>
2024-04-16 17:27:35 +0530

    Registration page.

---
+++ front-end/src/app/components/page.tsx
@@ -0,0 +1,19 @@
+// """src/app/auth/register/page.tsx: """
+
+
+// __author__ = 'Rajesh Pethe'
+// __date__ = '04/16/2024 17:19:12'
+// __credits__ = ['Rajesh Pethe']
+
+
+"use client";
+
+import Register from "@/app/components/Register";
+
+export default function Home() {
+    return (
+        <main>
+            <Register />
+        </main>
+    );
+}
