b60866fcae01bb9668a21cf5b90d8cc4321e79c8
Rajesh P <rajesh.pethe@gmail.com>
2024-04-16 18:59:32 +0530

    Forgot password and reset password.

---
+++ front-end/src/app/auth/password/reset-password-confirmation/page.tsx
@@ -0,0 +1,18 @@
+// """src/app/auth/password/reset-password-confirm/page.tsx: """
+
+
+// __author__ = 'Rajesh Pethe'
+// __date__ = '04/16/2024 18:52:57'
+// __credits__ = ['Rajesh Pethe']
+
+"use client";
+
+import ResetPasswordConfirmation from "@/app/components/ResetPasswordConfirmation";
+
+export default function Home() {
+    return (
+        <main>
+            <ResetPasswordConfirmation />
+        </main>
+    );
+}
---
+++ front-end/src/app/auth/password/reset-password/page.tsx
@@ -0,0 +1,19 @@
+// """src/app/auth/password/reset-password/page.tsx: """
+
+
+// __author__ = 'Rajesh Pethe'
+// __date__ = '04/16/2024 18:30:19'
+// __credits__ = ['Rajesh Pethe']
+
+
+"use client";
+
+import ResetPassword from "@/app/components/ResetPassword";
+
+export default function Home() {
+    return (
+        <main>
+            <ResetPassword />
+        </main>
+    );
+}
